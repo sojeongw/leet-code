eac1c1a08f5d43592152dd2d90be6b09350c61bc
dodeon <33208821+sojeongw@users.noreply.github.com>
2022-03-22 16:41:11 +0900

    Time: 915 ms (33.33%), Space: 23.3 MB (71.43%) - LeetHub

---
+++ 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minElements(self, nums, limit, goal):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :type goal: int
+        :rtype: int
+        """
+        sum = 0
+        
+        for i in nums:
+            sum += i
+            
+        diff = abs(goal - sum)
+        
+        if diff == 0:
+            return 0
+        
+        if diff%limit == 0:
+            return diff//limit
+        
+        return (diff//limit) + 1
