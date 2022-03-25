99e4de4e8e4d1fd8783bf5842e357242b1941683
dodeon <33208821+sojeongw@users.noreply.github.com>
2022-03-25 19:59:36 +0900

    Time: 32 ms (97.19%), Space: 13.9 MB (26.52%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.py
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.py
@@ -6,7 +6,7 @@ class Solution:
             if value.isdecimal() and value not in result:
                 result.append(value)
 
-        if len(result) == 0 or len(result) == 1:
+        if len(result) < 2:
             return -1
         
         result.sort()
